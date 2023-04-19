8cb6146fd40a1d0d0089fbdfea504f6729ca9dcd
Fernando Bustillos <bustillos007@gmail.com>
2023-04-19 12:38:11 -0400

    Update event listeners

--- script.js
+++ script.js
@@ -3,16 +3,18 @@ const inputBtns = document.querySelectorAll("button")
 const clearBtn = document.getElementById('clear-btn')
 
 function sendNumberValue(number){
-    console.log(number)
+//    if current display value is 0, replace it, if not add number
+const displayValue = calculatorDisplay.textContent;
+calculatorDisplay.textContent = displayValue === "0"
 }
 
 // Add event listeners for numbers, operators, decimal buttons
 inputBtns.forEach((inputBtn)=>{
     if(inputBtn.classList.length === 0){
-        inputBtn.addEventListener('click', sendNumberValue(inputBtn.value))
+        inputBtn.addEventListener('click', ()=>sendNumberValue(inputBtn.value))
     }else if (inputBtn.classList.contains('operator')){
-        inputBtn.addEventListener('click', sendNumberValue(inputBtn.value))
+        inputBtn.addEventListener('click', ()=>sendNumberValue(inputBtn.value))
     }  else if (inputBtn.classList.contains('decimal')){
-    inputBtn.addEventListener('click', sendNumberValue(inputBtn.value))
+    inputBtn.addEventListener('click', ()=>sendNumberValue())
     }
 })
